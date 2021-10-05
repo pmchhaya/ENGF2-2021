4389c0a8721b2b1e6925a6b891ea21eb544c0807
Mark Handley <mark@handley.org.uk>
2021-10-05 10:44:35 +0100

    Remove bg="white" so tkinter_test works OK in dark mode.

--- assignments/assignment1/tkinter_test.py
+++ assignments/assignment1/tkinter_test.py
@@ -9,7 +9,7 @@ CANVAS_HEIGHT = 200
 
 rootwindow = Tk();
 rootwindow.bind_all('<Key>', key)
-canvas = Canvas(rootwindow, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="white")
+canvas = Canvas(rootwindow, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
 canvas.pack(side = LEFT, fill=BOTH, expand=TRUE)
 textwidget = canvas.create_text(150, 100, anchor="c")
 version = rootwindow.tk.call("info", "patchlevel")
