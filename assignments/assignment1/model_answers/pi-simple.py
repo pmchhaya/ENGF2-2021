8b6923890ec068eb9cef6451d20390066d2fe643
Mark Handley <mark@handley.org.uk>
2021-10-11 23:02:07 +0100

    Add model answers for assignment 1

---
+++ assignments/assignment1/model_answers/pi-simple.py
@@ -0,0 +1,19 @@
+import random
+
+def estimate_pi(iterations):
+    in_counts = 0
+
+    for i in range(iterations):
+        x = random.uniform(0,1) - 0.5
+        y = random.uniform(0,1) - 0.5
+        r_squared = x*x + y*y
+        if r_squared < 0.25:
+            in_counts += 1
+
+    # area of circle, area_c = pi*r^2 = pi
+    # area of square, area_sq = 4
+    # ratio_of_counts ~= area_c / area_sq = pi / 4
+    # => pi ~= 4 * ratio_of_counts
+    estimates = 4 * in_counts/iterations
+
+print(estimate_pi(10000))
